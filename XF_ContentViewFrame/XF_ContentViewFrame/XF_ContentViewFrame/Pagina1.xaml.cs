0a8a83c964a42e67bbe88c3b492b0884c42594c0
luiz marcello <luizmarcello.vmo@hotmail.com>
2020-06-27 10:47:07 -0300

    27.06.2020_10:46hs

---
+++ XF_ContentViewFrame/XF_ContentViewFrame/XF_ContentViewFrame/Pagina1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_ContentViewFrame
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina1 : ContentPage
+    {
+        public Pagina1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Pagina3());
+        }
+    }
+}
